ddc345d5576781e04d7e4f66eb29cfa5aa3f6e71
Chris Woodall <cwoodall@bu.edu>
2013-08-21 17:37:34 -0400

    Updated to new MODBUS inspired UART protocol

--- Debug/main.pp
+++ Debug/main.pp
@@ -5,17 +5,17 @@ main.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430.h
 main.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430g2553.h
 main.obj: C:/ti/ccsv5/ccs_base/msp430/include/in430.h
 main.obj: C:/ti/ccsv5/tools/compiler/msp430_4.1.5/include/stdint.h
-main.obj: C:/Users/Admin/workspace_v5_4/msp430-usci-uart/include/uart/uart.h
-main.obj: C:/Users/Admin/workspace_v5_4/msp430-usci-uart/include/spi/spi.h
-main.obj: C:/Users/Admin/workspace_v5_4/msp430-usci-uart/include/spi/dac7512.h
-main.obj: C:/Users/Admin/workspace_v5_4/msp430-usci-uart/include/spi/ad5504.h
+main.obj: C:/Users/Admin/workspace_v5_4/nasa-mux-16ch-firmware/include/uart/uart.h
+main.obj: C:/Users/Admin/workspace_v5_4/nasa-mux-16ch-firmware/include/spi/spi.h
+main.obj: C:/Users/Admin/workspace_v5_4/nasa-mux-16ch-firmware/include/spi/dac7512.h
+main.obj: C:/Users/Admin/workspace_v5_4/nasa-mux-16ch-firmware/include/spi/ad5504.h
 
 ../main.c: 
 C:/ti/ccsv5/ccs_base/msp430/include/msp430.h: 
 C:/ti/ccsv5/ccs_base/msp430/include/msp430g2553.h: 
 C:/ti/ccsv5/ccs_base/msp430/include/in430.h: 
 C:/ti/ccsv5/tools/compiler/msp430_4.1.5/include/stdint.h: 
-C:/Users/Admin/workspace_v5_4/msp430-usci-uart/include/uart/uart.h: 
-C:/Users/Admin/workspace_v5_4/msp430-usci-uart/include/spi/spi.h: 
-C:/Users/Admin/workspace_v5_4/msp430-usci-uart/include/spi/dac7512.h: 
-C:/Users/Admin/workspace_v5_4/msp430-usci-uart/include/spi/ad5504.h: 
+C:/Users/Admin/workspace_v5_4/nasa-mux-16ch-firmware/include/uart/uart.h: 
+C:/Users/Admin/workspace_v5_4/nasa-mux-16ch-firmware/include/spi/spi.h: 
+C:/Users/Admin/workspace_v5_4/nasa-mux-16ch-firmware/include/spi/dac7512.h: 
+C:/Users/Admin/workspace_v5_4/nasa-mux-16ch-firmware/include/spi/ad5504.h: 
--- Debug/src/spi/dac7512.pp
+++ Debug/src/spi/dac7512.pp
@@ -2,16 +2,16 @@
 
 src/spi/dac7512.obj: ../src/spi/dac7512.c
 src/spi/dac7512.obj: C:/ti/ccsv5/tools/compiler/msp430_4.1.5/include/stdint.h
-src/spi/dac7512.obj: C:/Users/Admin/workspace_v5_4/msp430-usci-uart/include/spi/spi.h
+src/spi/dac7512.obj: C:/Users/Admin/workspace_v5_4/nasa-mux-16ch-firmware/include/spi/spi.h
 src/spi/dac7512.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430.h
 src/spi/dac7512.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430g2553.h
 src/spi/dac7512.obj: C:/ti/ccsv5/ccs_base/msp430/include/in430.h
-src/spi/dac7512.obj: C:/Users/Admin/workspace_v5_4/msp430-usci-uart/include/spi/dac7512.h
+src/spi/dac7512.obj: C:/Users/Admin/workspace_v5_4/nasa-mux-16ch-firmware/include/spi/dac7512.h
 
 ../src/spi/dac7512.c: 
 C:/ti/ccsv5/tools/compiler/msp430_4.1.5/include/stdint.h: 
-C:/Users/Admin/workspace_v5_4/msp430-usci-uart/include/spi/spi.h: 
+C:/Users/Admin/workspace_v5_4/nasa-mux-16ch-firmware/include/spi/spi.h: 
 C:/ti/ccsv5/ccs_base/msp430/include/msp430.h: 
 C:/ti/ccsv5/ccs_base/msp430/include/msp430g2553.h: 
 C:/ti/ccsv5/ccs_base/msp430/include/in430.h: 
-C:/Users/Admin/workspace_v5_4/msp430-usci-uart/include/spi/dac7512.h: 
+C:/Users/Admin/workspace_v5_4/nasa-mux-16ch-firmware/include/spi/dac7512.h: 
